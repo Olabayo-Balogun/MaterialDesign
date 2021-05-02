3866e74f3a83dea52780d4fb6859eff1117c8a78
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-05-02 18:52:43 +0100

    Adding functionality to spinner

--- app/src/main/java/com/example/materialdesign/MainActivity.kt
+++ app/src/main/java/com/example/materialdesign/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.materialdesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +18,20 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupSimpleSpinner() {
         val spinner = findViewById<Spinner>(R.id.simple_spinner)
+
+        val adapter = ArrayAdapter.createFromResource(this, R.array.currency, android.R.layout.simple_spinner_item)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedItem = parent!!.getItemAtPosition(position)
+                Toast.makeText(this@MainActivity, "$selectedItem", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                Toast.makeText(this@MainActivity, "No item was selected", Toast.LENGTH_LONG).show()
+            }
+
+        }
     }
 }
