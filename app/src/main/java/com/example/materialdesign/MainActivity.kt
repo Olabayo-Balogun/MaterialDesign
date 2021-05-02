96d500aa89406d2a872cdcb9514ddf6fdc2adcb5
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-05-02 18:10:36 +0100

    Creating the spinner

--- app/src/main/java/com/example/materialdesign/MainActivity.kt
+++ app/src/main/java/com/example/materialdesign/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.materialdesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupSimpleSpinner()
+    }
+
+    private fun setupSimpleSpinner() {
+        val spinner = findViewById<Spinner>(R.id.simple_spinner)
     }
 }
