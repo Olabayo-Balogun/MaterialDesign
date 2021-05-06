d2ad35c1329b49a4f7588084d9e9950c9ac60d0b
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-05-06 16:18:22 +0100

    Adding intent

--- app/src/main/java/com/example/materialdesign/MainActivity.kt
+++ app/src/main/java/com/example/materialdesign/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.materialdesign
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,31 +8,53 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupSimpleSpinner()
+        signIn()
+//        setupSimpleSpinner()
     }
 
-    private fun setupSimpleSpinner() {
-        val spinner = findViewById<Spinner>(R.id.simple_spinner)
+    private fun signIn() {
+        val email = findViewById<TextInputEditText>(R.id.email_edit_text)
+        val password = findViewById<TextInputEditText>(R.id.password_edit_text)
+        val signInButton = findViewById<MaterialButton>(R.id.next_button)
 
-        val adapter = ArrayAdapter.createFromResource(this, R.array.currency, android.R.layout.simple_spinner_item)
-        spinner.adapter = adapter
-
-        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val selectedItem = parent!!.getItemAtPosition(position)
-                Toast.makeText(this@MainActivity, "$selectedItem", Toast.LENGTH_LONG).show()
+        signInButton.setOnClickListener(){
+            if(email.toString().isNullOrEmpty() || password.toString().isNullOrEmpty()){
+                Toast.makeText(this, "Email or Password is not provided", Toast.LENGTH_LONG).show()
+            } else{
+                if(email.toString() == "man@gmail.com" && password.toString() == "12345"){
+                    val intent = Intent(this, ViewActivity::class.java)
+                    startActivity(intent)
+                }else{
+                    Toast.makeText(this, "Wrong email or password", Toast.LENGTH_LONG).show()
+                }
             }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                Toast.makeText(this@MainActivity, "No item was selected", Toast.LENGTH_LONG).show()
-            }
-
         }
     }
+
+//    private fun setupSimpleSpinner() {
+//        val spinner = findViewById<Spinner>(R.id.simple_spinner)
+//
+//        val adapter = ArrayAdapter.createFromResource(this, R.array.currency, android.R.layout.simple_spinner_item)
+//        spinner.adapter = adapter
+//
+//        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+//            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+//                val selectedItem = parent!!.getItemAtPosition(position)
+//                Toast.makeText(this@MainActivity, "$selectedItem", Toast.LENGTH_LONG).show()
+//            }
+//
+//            override fun onNothingSelected(parent: AdapterView<*>?) {
+//                Toast.makeText(this@MainActivity, "No item was selected", Toast.LENGTH_LONG).show()
+//            }
+//
+//        }
+//    }
 }
